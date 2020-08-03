8311fdfacdb3b6af174e556ced5b657889dafa25
Manoj Reddy <manojeee251@gmail.com>
2020-08-03 08:56:51 +0530

    First Commit

--- TestOne.py
+++ TestOne.py
@@ -1,1 +1,12 @@
 print("hello world")
+if 5 > 2:
+ print("Five is greater than two!")
+ # comment 1
+ # comment 2
+ # comment 3
+ print("Five is greater than two!")
+
+ """ 
+ Commenting code
+ """
+print("Five is greater than two!")
